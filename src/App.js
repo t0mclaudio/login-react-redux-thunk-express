4a8288706cb3c855e2f4c3a50f2e1045ab99500a
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-22 20:15:40 +0800

    Import and Mount Login Module in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Login from './Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login />
       </div>
     );
   }
