e57db4144d7aa1681d375c018d018bfaf1f7031b
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 14:06:17 +0800

    Add label to inputs and wrap inputs with divs

--- src/Login.js
+++ src/Login.js
@@ -10,10 +10,25 @@ export default class Login extends Component {
   render() {
     return (
       <form>
-        <input type="text" name="username" value={this.state.username}/>
-        <input type="password" name="password" value={this.state.password}/>
-        <input type="checkbox" name="keepLoggedIn" checked={this.state.keepLoggedIn}/>
-        <input type="submit" value="Log In" />
+        <div>
+          <label> Username
+            <input type="text" name="username" value={this.state.username}/>
+          </label>
+        </div>
+        <div>
+          <label> Password
+            <input type="password" name="password" value={this.state.password}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" name="keepLoggedIn" checked={this.state.keepLoggedIn}/>
+            Keep logged in
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Log In" />
+        </div>  
       </form>
     )
   }
