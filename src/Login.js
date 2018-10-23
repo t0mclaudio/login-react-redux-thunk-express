27ff5703046a33e5d172eebe552f0c9942c814e9
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 17:10:42 +0800

    Use handleChange method from props to handle change event

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
 export default class Login extends Component {
+  handleChange(e){
+    this.props.handleChange(e)
+  }
+
   handleLogIn(e) {
     e.preventDefault()
     this.props.handleLogIn()
-  }
-
-  toggleKeepLoggedIn(e) {
-    this.handleLogIn(e)
   }
   
   render() {
@@ -15,14 +15,14 @@ export default class Login extends Component {
       <form onSubmit={e => this.handleLogIn(e)}>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" value={this.props.data.username} onChange={e => this.setState({username: e.target.value})} />
+          <input type="text" name="username" value={this.props.data.username} onChange={e => this.handleChange(e)} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={this.props.data.password} onChange={e => this.setState({password: e.target.value})}/>
+          <input type="password" name="password" value={this.props.data.password} onChange={e => this.handleChange(e)}/>
         </div>
         <div>  
-          <input type="checkbox" name="keepLoggedIn" id="keepLoggedIn" checked={this.props.data.keepLoggedIn} onChange={e => this.toggleKeepLoggedIn(e)}/>
+          <input type="checkbox" name="keepLoggedIn" id="keepLoggedIn" checked={this.props.data.keepLoggedIn} onChange={e => this.handleChange(e)}/>
           <label htmlFor="keepLoggedIn">Keep Logged in</label>
         </div>
         <div>
