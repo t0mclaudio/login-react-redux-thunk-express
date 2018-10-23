f86e4215cd60cf7db19d3f74957bc83fc69017d7
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 14:44:15 +0800

    Change for to htmlFor for correct JSX syntax

--- src/Login.js
+++ src/Login.js
@@ -11,16 +11,16 @@ export default class Login extends Component {
     return (
       <form>
         <div>
-          <label for="username">Username</label>
+          <label htmlFor="username">Username</label>
           <input type="text" name="username" value={this.state.username} onChange={e => this.setState({username: e.target.value})} />
         </div>
         <div>
-          <label for="password">Password</label>
+          <label htmlFor="password">Password</label>
           <input type="password" name="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
         </div>
         <div>  
           <input type="checkbox" name="keepLoggedIn" id="keepLoggedIn" checked={this.state.keepLoggedIn} onChange={e => this.setState({keepLoggedIn: !this.state.keepLoggedIn})}/>
-          <label for="keepLoggedIn">Keep Logged in</label>
+          <label htmlFor="keepLoggedIn">Keep Logged in</label>
         </div>
         <div>
           <input type="submit" value="Log In" />
