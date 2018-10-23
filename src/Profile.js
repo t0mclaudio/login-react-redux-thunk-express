43ed11096c441ac04ab533b8a4bb667cf3ee2480
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 14:55:35 +0800

    Create a Profile Module

---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default const ProfilePage = props => {
+  return (
+    <div>
+      <h1>Name: Tom Claudio</h1>
+      <h2>Title: Software Developer</h2>
+    </div>
+  )
+}
