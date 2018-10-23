9bc89284968c84108e05707d15eb4a028868b9a8
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 16:13:02 +0800

    Import createStore from redux; create store, create Application component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { createStore }  from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+const Application = () => {
+  return (
+  <div>
+    <App />
+  </div>
+  )
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'));
