3fa36d72e6123527a48a91ef02546289533a2238
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 15:07:33 +0800

    Rename module name; fix module exporting

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default const ProfilePage = props => {
+const Profile = props => {
   return (
     <div>
       <h1>Name: Tom Claudio</h1>
       <h2>Title: Software Developer</h2>
     </div>
   )
-}
+};
+
+export default Profile;
