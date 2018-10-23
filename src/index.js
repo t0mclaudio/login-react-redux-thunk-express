1a70efaa96b5d9bb81bcf730020973cfe46b70ff
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 16:18:34 +0800

    Create reducer function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import App from './App';
 import { createStore }  from 'redux';
 import { Provider } from "react-redux";
 
-const store = createStore();
+const store = createStore(reducer);
+
+function reducer(state, action) {
+  return {
+    data: null
+  }
+}
 
 const Application = () => {
   return (
