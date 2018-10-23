05270ade20232aea06415492c45fc74da9c5a196
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 14:11:19 +0800

    Add onChange method on Inputs; Refactor input labels

--- src/Login.js
+++ src/Login.js
@@ -11,20 +11,16 @@ export default class Login extends Component {
     return (
       <form>
         <div>
-          <label> Username
-            <input type="text" name="username" value={this.state.username}/>
-          </label>
+          <label for="username">Username</label>
+          <input type="text" name="username" value={this.state.username} onChange={e => this.setState({username: e.target.value})} />
         </div>
         <div>
-          <label> Password
-            <input type="password" name="password" value={this.state.password}/>
-          </label>
+          <label for="password">Password</label>
+          <input type="password" name="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
         </div>
-        <div>
-          <label>
-            <input type="checkbox" name="keepLoggedIn" checked={this.state.keepLoggedIn}/>
-            Keep logged in
-          </label>
+        <div>  
+          <input type="checkbox" name="keepLoggedIn" checked={this.state.keepLoggedIn} onChange={e => this.setState({keepLoggedIn: !this.state.keepLoggedIn})}/>
+          <label for="keepLoggedIn">Keep Logged in</label>
         </div>
         <div>
           <input type="submit" value="Log In" />
