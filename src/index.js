ba608530356be72f80a522589bd21d9a450d338d
Tom Claudio <t0mclaudio@Toms-Imac.local>
2018-10-23 16:14:48 +0800

    Wrap Application with Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore }  from 'redux';
+import { Provider } from "react-redux";
 
 const store = createStore();
 
 const Application = () => {
   return (
-  <div>
-    <App />
-  </div>
+    <Provider store={store} >
+      <App />
+    </Provider>
   )
 }
 
